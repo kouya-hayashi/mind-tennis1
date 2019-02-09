409c8320fa05214cac7248614d3cd099cdb9022a
kouya hayashi <toripurug.884@gmail.com>
2019-02-10 07:47:24 +0900

    login add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,15 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to root_path, success: "登録が完了しました"
     else
-      flash.now[:alert] = "登録に失敗しました"
+      flash.now[:danger] = "登録に失敗しました"
       render :new
     end
   end
 
+ #KCLASSの外部からは呼び出せない
+ #Class内でしか実行されないメソッドを使用
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   #フォームが空白かを判断
-  validates :name, presence: true
-  validates :email, presence: true
+  #validationの途中　パスワードとメールアドレス
+  validates :name, presence: true#/^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}$/i
+  validates :email, presence: true, length: { minimum: 8, maximum: 32}
+
+#パスワード機能の実装
+  has_secure_password
 end
